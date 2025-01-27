e6901d6dc6a79966d500a961a4f032226798b5cd
Aravindha Loshan SenthilKumar <143547399+Aravindhaloshan@users.noreply.github.com>
2025-01-27 09:32:07 +0100

    model.py

--- model.py
+++ model.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 
 
 class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, in_size, hid_size, num_class):
         super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size) 
-        self.l2 = nn.Linear(hidden_size, hidden_size) 
-        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.l1 = nn.Linear(in_size, hid_size) 
+        self.l2 = nn.Linear(hid_size, hid_size) 
+        self.l3 = nn.Linear(hid_size, num_class)
         self.relu = nn.ReLU()
     
-    def forward(self, x):
-        out = self.l1(x)
+    def forward(self, a):
+        out = self.l1(a)
         out = self.relu(out)
         out = self.l2(out)
         out = self.relu(out)
