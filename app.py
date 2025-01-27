8a0182ac703945203ec981fde88b9fc1f77aa477
Aravindha Loshan SenthilKumar <143547399+Aravindhaloshan@users.noreply.github.com>
2025-01-27 09:34:39 +0100

    app.py

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ from chat import get_response
 
 app = Flask(__name__)
 
-
-@app.get("/")
-def index_get(): 
+@app.route("/")
+def index_get():
     return render_template("base.html")
+
+@app.route("/about")
+def about_get():
+    return render_template("about.html")
+
+@app.route("/events")
+def events_get():
+    return render_template("events.html")
+
+@app.route("/course")
+def course_get():
+    return render_template("course.html")
+
+@app.route("/contact")
+def contact_get():
+    return render_template("contact.html")
 
 @app.post("/predict")
 def predict():
@@ -16,7 +31,6 @@ def predict():
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message) 
-
 
 if __name__ == "__main__":
     app.run(debug=True)
